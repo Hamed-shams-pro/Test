35e9bb8c751675898aab228462cce3c8878b943d
Hamed Shams <splinterjunior4@gmail.com>
2025-02-25 20:41:20 +0330

    commit 03

---
+++ CreateGenericClass/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using CreateGenericClass;
+
+Student student = new Student();
+student.Id = 2147099990;
+student.Name = "Hamed";
+student.Family = "Shams";
+student.PrintInfoAsync();
+student.GetAgeAsync(19);
+
+//Teacher teacher = new Teacher();
+//teacher.Id = 255;
+Console.ReadLine();
---
+++ CreateGenericClass/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateGenericClass
+{
+    internal class Student : BaseEntity<int>
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+        private int _age;
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Before (Id {Id} , Name : {Name} , Family : {Family})");
+            Thread.Sleep(5000);
+            Console.WriteLine($"After (Id {Id} , Name : {Name} , Family : {Family})");
+        }
+        public void GetAge(int age)
+        {
+            if (age > 18)
+            {
+                _age = age;
+                Console.WriteLine(_age);
+            }
+        }
+        public async Task PrintInfoAsync()
+        {
+            Console.WriteLine($"Before (Id {Id} , Name : {Name} , Family : {Family})");
+           await Task.Delay(5000);
+            Console.WriteLine($"After (Id {Id} , Name : {Name} , Family : {Family})");
+        }
+        public async Task GetAgeAsync(int age)
+        {
+            if (age > 18)
+            {
+                _age = age;
+                Console.WriteLine(_age);
+            }
+        }
+    }
+}
